191fabafccb38a83fdf5f9991e477e41977808b6
robhattarai <robhattarai@outlook.com>
2019-09-13 03:38:31 -0500

    Integrated tests with SauceLabs and BrowserStack

--- Automation/Selenium/TestInit.cs
+++ Automation/Selenium/TestInit.cs
@@ -1,30 +1,55 @@
-﻿using NUnit.Framework;
+﻿using System;
 using log4net;
 using log4net.Config;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using Selenium;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System.Collections.Generic;
 
 namespace Selenium
 {
     [TestFixture]
+    [Category("SauceLabTest"), Category("NUnit")]
     public class TestInit
     {
         public static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public IWebDriver driver;
-        protected AppVariables appVar;
-
-        private string URL = "https://www.amazon.com/";
-
+ 
         [SetUp]
         public void SetUp()
         {
+            log.Info("Open Browser and Navigate URL: " + AppVariables.URL);
             XmlConfigurator.Configure();
-            appVar = new AppVariables();
-            driver = new ChromeDriver();
+            //driver = new ChromeDriver();
+            driver = SauceLabsConnectUsingBrowserSpecificOptions();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
             driver.Manage().Window.Maximize();
-            log.Info("Open Browser and Navigate URL: " + URL);
-            driver.Url = appVar.GetURL();
+            driver.Url = AppVariables.GetURL();
+        }
+
+        public IWebDriver BrowserStackConnectUsingBrowserSpecificOptions()
+        {
+            var browserOptions = new ChromeOptions();
+            browserOptions.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name, true);
+            browserOptions.AddAdditionalCapability("browserstack.user", AppVariables.BROWSERSTACK_USERNAME, true);
+            browserOptions.AddAdditionalCapability("browserstack.key", AppVariables.BROWSERSTACK_ACCESSKEY, true);
+            driver = new RemoteWebDriver(new Uri("http://hub-cloud.browserstack.com/wd/hub/"), browserOptions);
+            return driver;
+        }
+
+        public IWebDriver SauceLabsConnectUsingBrowserSpecificOptions()
+        {
+            var sauceOptions = new Dictionary<string, object>();
+
+            var browserOptions = new ChromeOptions();
+            browserOptions.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name, true);
+            browserOptions.AddAdditionalCapability("username", AppVariables.GetSAUCELABS_USERNAME(), true);
+            browserOptions.AddAdditionalCapability("accessKey", AppVariables.GetSAUCELABS_ACCESSKEY(), true);
+            driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), browserOptions);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            return driver;
         }
 
         [TearDown]
--- Automation/Selenium/Utilities/AppVariables.cs
+++ Automation/Selenium/Utilities/AppVariables.cs
@@ -6,7 +6,15 @@ namespace Selenium
     public class AppVariables
     {
         public static string URL { get; set; }
+        public static string SAUCELABS_USERNAME { get; set; }
+        public static string SAUCELABS_ACCESSKEY { get; set; }
+        public static string BROWSERSTACK_USERNAME { get; set; }
+        public static string BROWSERSTACK_ACCESSKEY { get; set; }
 
-        public string GetURL() { return ConfigurationManager.AppSettings["URL"]; }
+        public static string GetURL() { return ConfigurationManager.AppSettings["URL"]; }
+        public static string GetSAUCELABS_USERNAME() { return ConfigurationManager.AppSettings["SAUCELABS_USERNAME"]; }
+        public static string GetSAUCELABS_ACCESSKEY() { return ConfigurationManager.AppSettings["SAUCELABS_ACCESSKEY"]; }
+        public static string GetBROWSERSTACK_USERNAME() { return ConfigurationManager.AppSettings["BROWSERSTACK_USERNAME"]; }
+        public static string GetBROWSERSTACK_ACCESSKEY() { return ConfigurationManager.AppSettings["BROWSERSTACK_ACCESSKEY"]; }
     }
 }
