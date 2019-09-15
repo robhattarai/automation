7a3880bec9147a95775b4bb3c5217d306c5202a1
robhattarai <robhattarai@outlook.com>
2019-09-15 10:16:07 -0500

    Fixed session timeout issue

--- Automation/Selenium/TestInit.cs
+++ Automation/Selenium/TestInit.cs
@@ -36,15 +36,15 @@ namespace Selenium
             ChromeOptions browserOptions = new ChromeOptions();
             browserOptions.AddAdditionalCapability("os", "Windows", true);
             browserOptions.AddAdditionalCapability("browser", "chrome", true);
- //           browserOptions.AddAdditionalCapability("browserstack.tunnel", false, true);
+            //browserOptions.AddAdditionalCapability("browserstack.tunnel", true, true);
             browserOptions.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name, true);
             browserOptions.AddAdditionalCapability("browserstack.user", AppVariables.GetBROWSERSTACK_USERNAME(), true);
             browserOptions.AddAdditionalCapability("browserstack.key", AppVariables.GetBROWSERSTACK_ACCESS_KEY(), true);
- //           browserOptions.AddAdditionalCapability("browserstack.local", AppVariables.GetBROWSERSTACK_LOCAL(), true);
-//            browserOptions.AddAdditionalCapability("browserstack.localIdentifier", AppVariables.GetBROWSERSTACK_LOCAL_IDENTIFIER(), true);
-            driver = new RemoteWebDriver(new Uri("http://hub-cloud.browserstack.com/wd/hub/"), browserOptions);
-            //driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub/"), browserOptions);
-//            driver = new RemoteWebDriver(new Uri("https://" + AppVariables.BROWSERSTACK_USERNAME + ":" + AppVariables.BROWSERSTACK_ACCESSKEY + "@hub-cloud.browserstack.com/wd/hub/"), browserOptions);
+            //browserOptions.AddAdditionalCapability("browserstack.local", AppVariables.GetBROWSERSTACK_LOCAL(), true);
+            //browserOptions.AddAdditionalCapability("browserstack.localIdentifier", AppVariables.GetBROWSERSTACK_LOCAL_IDENTIFIER(), true);
+//          driver = new RemoteWebDriver(new Uri("http://hub-cloud.browserstack.com/wd/hub/"), browserOptions);
+//          driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub/"), browserOptions);
+            driver = new RemoteWebDriver(new Uri("https://" + AppVariables.GetBROWSERSTACK_USERNAME() + ":" + AppVariables.GetBROWSERSTACK_ACCESS_KEY()+ "@hub-cloud.browserstack.com/wd/hub/"), browserOptions);
             return driver;
         }
 
@@ -66,6 +66,7 @@ namespace Selenium
         {
             log.Info("Close Browser instance");
             driver.Close();
+            driver.Quit();
         }
     }
 }
